91eca472a30a9b8edf76457b62751c5404117ef3
madeindreams <madeindreams@protonmail.com>
2025-12-08 19:41:41 -0800

    port update

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (!message || message.type !== "QUANTUMAUTH_REQUEST") return;
 
-    fetch("http://127.0.0.1:8090/extension/auth", {
+    fetch("http://127.0.0.1:6137/extension/auth", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(message.payload || {})
